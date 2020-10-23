2089c58fbf824f89d8dcded3df83e3ee66dd3cd1
repl.it user <replituser@example.com>
2020-10-23 04:24:21 +0000

    두번째날20201023

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-  printf("%s","life is egg");
+  printf("%s","두번째 날ㅎ");
   return 0;
 }
+// git log --online
+//         --graph
+// git status
+// git add-A
+// git status
+// git commit - ""
+// git status
