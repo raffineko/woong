ce50cb4569b96a10d7eb140d690d6bb65386ee00
repl.it user <replituser@example.com>
2020-10-23 04:51:31 +0000

    세번째

--- main.c
+++ main.c
@@ -12,4 +12,11 @@ int main(){
 // git add-A
 // git status
 // git commit - ""
-// git status
+// git push origin master
+// 아이디/비번
+
+
+//git diff
+
+//git reset +코드 모두 지우고 돌아감[개인용]  
+//git  revert +코드 1,2,(3,4) 2번으로 돌아가고 (3,4)건너뛰고  5번을 제작
